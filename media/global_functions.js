e0bfbb4c8336f4f760e2332b399b06ac313a5ca8
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-31 03:37:19 -0500

    Reworked tinymce to allow multiple addon bars

--- media/global_functions.js
+++ media/global_functions.js
@@ -11,14 +11,14 @@ tinymce_default_css_files[tinymce_default_css_files.length]
     = $_FULL_ROOT_PATH + '/youtube_for_bardcanvas/media/global_styles~v' + $_SCRIPTS_VERSION + '.css';
 
 // Callback for the button below the editor
-$_TINYMCE_ADDON_FUNCTIONS['embed_youtube_link_in_post_editor'] = function($trigger, $form)
+$_TINYMCE_ADDON_FUNCTIONS['embed_youtube_link_in_post_editor'] = function($trigger, $container)
 {
     var $strings = $('#youtube_for_bardcanvas_tinymce_strings');
     var _title    = $strings.find('.title').text();
     var _caption  = $strings.find('.caption').text();
     var _invalid  = $strings.find('.invalid_link').text();
     
-    var editor_id = $form.find('textarea[class*="tinymce"]').attr('id');
+    var editor_id = $container.find('textarea[class*="tinymce"]').attr('id');
     var editor    = tinymce.get(editor_id);
     editor.windowManager.open({
         title: _title,
